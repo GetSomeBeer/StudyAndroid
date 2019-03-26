2f4cf96da77353ad583b2340ebd767c93d20aaf7
atlas <atlas9539@gmail.com>
2019-03-26 19:25:18 +0900

    [init] create project

---
+++ app/src/main/java/study/com/hiandroid/MainActivity.java
@@ -0,0 +1,18 @@
+package study.com.hiandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Dongju on 26/03/2019.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
